6a9809ac77e050296a1fd082bc206e8c65daae19
lopatan <natalia.lopata@contractors.roche.com>
2025-04-29 12:15:33 +0200

    no flights scenarios implemented

--- src/test/java/net/phptravels/FlightsCommon.java
+++ src/test/java/net/phptravels/FlightsCommon.java
@@ -6,6 +6,8 @@ import net.phptravels.pages.SearchPanel;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 
+import java.util.List;
+
 @Slf4j
 public class FlightsCommon extends BaseTest {
     private String fromCity1Code = "LON";
@@ -22,9 +24,14 @@ public class FlightsCommon extends BaseTest {
                 log.info("Found 'TO' city");
                 searchPanel.clickSearchFlight();
                 FoundFlightsPage foundFlightsPage = new FoundFlightsPage(driver);
-                flightEl = foundFlightsPage.waitForFLightsList().getFlights().get(0);
-
-                Assert.assertTrue(flightEl.isDisplayed());
+                if (checkIfFlightsFound()) {
+                    log.info("Flights found.");
+                    flightEl = foundFlightsPage.waitForFLightsList().getFlights().get(0);
+                    Assert.assertTrue(flightEl.isDisplayed());
+                } else {
+                    log.info("Flights not found. Assertion for flights results skipped.");
+                    return null;
+                }
             } else {
                 log.info("NOT Found 'TO' city. Test skipped.");
                 return null;
@@ -34,6 +41,21 @@ public class FlightsCommon extends BaseTest {
             return null;
         }
         return flightEl;
+    }
+
+    public boolean checkIfFlightsFound() {
+        FoundFlightsPage foundFlightsPage = new FoundFlightsPage(driver);
+        List<WebElement> flights = foundFlightsPage.waitForFlightsResults().getFlights();
+
+        if (!flights.isEmpty() && flights.get(0).isDisplayed()) {
+            log.info(String.format("Flights has shown (count: %s)", flights.size()));
+            return true;
+        }
+        if (foundFlightsPage.getFlightsInfo().equals("No Flights Available")) {
+            log.info("'No Flights Available' info has shown.");
+            return false;
+        }
+        return false;
     }
 
     public String getFromCity1Code() {
--- src/test/java/net/phptravels/FoundFlightsTest.java
+++ src/test/java/net/phptravels/FoundFlightsTest.java
@@ -1,33 +1,36 @@
 package net.phptravels;
 
+import lombok.extern.slf4j.Slf4j;
 import net.phptravels.pages.FoundFlightsPage;
 import org.assertj.core.api.SoftAssertions;
 import org.testng.annotations.Test;
 
+@Slf4j
 public class FoundFlightsTest extends FlightsCommon {
     @Test
     public void shouldResultsShowCorrectData() {
-        searchBasicFlight();
-        FoundFlightsPage flightsPage = new FoundFlightsPage(driver);
+        var flights = searchBasicFlight();
+        if (flights != null) {
+            FoundFlightsPage flightsPage = new FoundFlightsPage(driver);
+            int actualFlightsFoundCount = flightsPage.getFlightsCount();
+            String cityFrom = flightsPage.getFlightFromCity(0);
+            String cityTo = flightsPage.getFlightToCity(0);
+            String tripDuration = flightsPage.getTripDuration(0);
+            String airLine1 = flightsPage.getFlightAirline(0, 0);
+            String airLine2 = flightsPage.getFlightAirline(0, 1);
 
-        int actualFlightsFoundCount = flightsPage.getFlightsCount();
+            SoftAssertions assertions = new SoftAssertions();
 
-        String cityFrom = flightsPage.getFlightFromCity(0);
-        String cityTo = flightsPage.getFlightToCity(0);
-        String tripDuration = flightsPage.getTripDuration(0);
-        String airLine1 = flightsPage.getFlightAirline(0, 0);
-        String airLine2 = flightsPage.getFlightAirline(0, 1);
+            assertions.assertThat(actualFlightsFoundCount).isGreaterThan(0);
+            assertions.assertThat(cityFrom).isEqualTo(getFromCity1Code());
+            assertions.assertThat(cityTo).isEqualTo(getToCity1Code());
+            assertions.assertThat(tripDuration).matches("Trip Duration: \\d{1,2}:\\d{2}\nLayover time: \\d+h \\d+m");
+            assertions.assertThat(airLine1).matches("[A-Za-z ]+\\RFlight No\\. \\d+ - [A-Z]{3} - [A-Z]{3}");
+            assertions.assertThat(airLine2).matches("[A-Za-z ]+\\RFlight No\\. \\d+ - [A-Z]{3} - [A-Z]{3}");
 
-        SoftAssertions assertions = new SoftAssertions();
-
-        assertions.assertThat(actualFlightsFoundCount).isGreaterThan(0);
-        assertions.assertThat(cityFrom).isEqualTo(getFromCity1Code());
-        assertions.assertThat(cityTo).isEqualTo(getToCity1Code());
-        assertions.assertThat(tripDuration).matches("Trip Duration: \\d{1,2}:\\d{2}\nLayover time: \\d+h \\d+m");
-        assertions.assertThat(airLine1).matches("[A-Za-z ]+\\RFlight No\\. \\d+ - [A-Z]{3} - [A-Z]{3}");
-        assertions.assertThat(airLine2).matches("[A-Za-z ]+\\RFlight No\\. \\d+ - [A-Z]{3} - [A-Z]{3}");
-
-        assertions.assertAll();
-
+            assertions.assertAll();
+        } else {
+            log.info("FLights not found. Test skipped.");
+        }
     }
 }
